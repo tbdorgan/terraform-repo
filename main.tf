3e55d2633e68df3d15db08d5fcc0abef16bb0fc2
tbdarch <toshack.desai@gmail.com>
2025-04-19 12:48:53 +0100

    Github workflow

--- main.tf
+++ main.tf
@@ -99,8 +99,8 @@ resource "aws_iam_policy" "lambda_policy" {
           aws_secretsmanager_secret.sns_secret.arn,
           aws_lambda_function.csv_lambda.arn,
           #aws_cloudwatch_log_group.csv_lambda_log_group.arn
-          #"arn:aws:logs:*:*:log-group:/aws/lambda/CsvFileHandlerLambda*"
-          "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.csv_lambda.function_name}:*"
+          "arn:aws:logs:*:*:log-group:/aws/lambda/CsvFileHandlerLambda*"
+          #"arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.csv_lambda.function_name}:*"
         ]
       }
     ]
